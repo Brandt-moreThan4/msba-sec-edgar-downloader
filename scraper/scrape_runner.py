f34936866553de6b49d0dd2a231796112a894ad5
Brandt Green <61711534+Brandt-moreThan4@users.noreply.github.com>
2022-02-26 16:38:02 -0600

    log cleanup

--- scraper/scrape_runner.py
+++ scraper/scrape_runner.py
@@ -14,18 +14,15 @@ from sec_edgar_downloader import Downloader
 
 filing_types = ['10-K','10-Q']
 tickers = ['EIX', 'NRG','WMB', 'SWN','DPL']
-
-tickers = ['TREC']
-
+tickers = ['JPM', 'DIS','V']
+# tickers = ['TREC']
 
 start_date = "2000-01-01"
-start_date = "2015-01-01"
+start_date = "2018-01-01"
 end_date = "2021-01-01"
 
 
 downloader = Downloader("scraper")
-
-# log_path = Path() / 'scraper'  f'log_{str(datetime.datetime.now()).replace(" ","")}.csv'
 
 log_dict: dict = {'ticker':[],'cik':[],'filing_type':[],'period_end':[],'file_name':[],'url':[],'success':[]}
 
@@ -33,13 +30,13 @@ log_dict: dict = {'ticker':[],'cik':[],'filing_type':[],'period_end':[],'file_na
 for ticker in tickers:
     print(f'Getting ticker: {ticker}')
     for filing_type in filing_types:
-        
+        # Add a try/except here
         downloader.get2(filing_type, ticker, after=start_date, before=end_date,log_dict=log_dict)
 
 
-        
+# Save the log to a csv
 df_log = pd.DataFrame(log_dict)
-log_path = Path() / 'scraper' / f'log_{str(datetime.datetime.now()).replace(" ","-").replace(":","_")}.csv'
+log_path = Path() / 'scraper' / 'logs' / f'log_{str(datetime.datetime.now()).replace(" ","-").replace(":","_")}.csv'
 df_log.to_csv(log_path)
 
 
